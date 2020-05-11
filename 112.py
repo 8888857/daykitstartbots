b1f9ecc0fa2cdad5536be06c136e8a322aa9f6d9
8888857 <65164085+8888857@users.noreply.github.com>
2020-05-11 14:47:01 +0400

    Add files via upload

---
+++ 112.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+from discord import utils
+ 
+#импорт папки конфигов
+import config
+
+#установить префикс
+bot = commands.Bot(command_prefix='/')
+
+#команда /прав
+@bot.command()
+async def прав(ctx):#команда
+    await ctx.send(config.PRAV1) #текст который выведеться
+
+#команда/прав1
+@bot.command()
+async def прав1(ctx):#команда
+    await ctx.send(config.p1) #текст который выведеться
+
+#команда /прав2
+@bot.command()
+async def прав2(ctx):#команда
+    await ctx.send(config.p2) #текст который выведеться
+
+#командо помощь
+@bot.command()
+async def помощь(ctx):#команда
+    await ctx.send(config.o1) #текст который выведеться
+
+#команда /обр1
+@bot.command()
+async def обр1(ctx):#команда
+    await ctx.send(config.ob1) #текст который выведеться
+
+#команда /обр2
+@bot.command()
+async def обр2(ctx):#команда
+    await ctx.send(config.ob2) #текст который выведеться
+
+#команда /обр3
+@bot.command()
+async def обр3(ctx):#команда
+    await ctx.send(config.ob3) #текст который выведеться
+
+#команда /обр
+@bot.command()
+async def обр(ctx):#команда
+    await ctx.send(config.ob) #текст который выведеться
+#поле для токена бота
+bot.run(config.TOKEN)
