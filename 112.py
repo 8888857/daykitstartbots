1edd61d3ea263574476077ce9e86ff6d28371640
8888857 <65164085+8888857@users.noreply.github.com>
2020-05-12 20:59:50 +0400

    Add files via upload

--- 112.py
+++ 112.py
@@ -56,7 +56,7 @@ async def очистить(ctx, amount=100):
 
 @bot.command(pass_context=True)
 @has_permissions(manage_messages=True)
-async def сказать(ctx, arg = 'Всем привет'):
+async def озв(ctx, arg = ('ПРИВЕТ, Я РОБОТ DayKitStart`а'):
     await ctx.send(arg)
     await ctx.message.delete()
 
