ee08be2faf2920d11d832cfac1ddb68d8dff9e13
8888857 <65164085+8888857@users.noreply.github.com>
2020-05-14 13:25:51 +0400

    Add files via upload

--- 112.py
+++ 112.py
@@ -7,11 +7,11 @@ from discord.ext.commands import has_permissions
 import config
 
 #установить префикс
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='')
 
 #команда /прав
 @bot.command()
-async def прав(ctx):#команда
+async def правила(ctx):#команда
     await ctx.send(config.PRAV1) #текст который выведеться
 
 #команда/прав1
@@ -46,22 +46,37 @@ async def обр3(ctx):#команда
 
 #команда /обр
 @bot.command()
-async def обр(ctx):#команда
+async def образец(ctx):#команда
     await ctx.send(config.ob) #текст который выведеться
 
+#удаляет сообщения
 @bot.command(pass_context=True)
 @has_permissions(manage_messages=True)
 async def очистить(ctx, amount=100):
     await ctx.channel.purge(limit=amount)
 
+#вывоит сообщение от имени бота
 @bot.command(pass_context=True)
 @has_permissions(manage_messages=True)
 async def озв(ctx, arg = 'ПРИВЕТ, Я РОБОТ DayKitStart`а'):
     await ctx.send(arg)
     await ctx.message.delete()
 
+OPERATIONS = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y
+}
+@bot.event
+async def on_ready():
+    print('Logged on as Calculator')
+@bot.command()
+async def калк(ctx, a, operator, b):
+    await ctx.send("Ответ: " + str(OPERATIONS[operator](int(a), int(b))))
+
 #поле для токена бота
-#bot.run(config.TOKEN)
+#bot.run("")
 
 token = os.environ.get("BOT_TOKEN")     
 bot.run(str(token))
